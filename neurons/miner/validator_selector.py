0c8d12817984d20327efc59dcb52cfac2ee63709
hiep.nt <hiep.nt@teko.vn>
2024-05-31 01:34:23 +0700

    .

--- neurons/miner/validator_selector.py
+++ neurons/miner/validator_selector.py
@@ -71,7 +71,7 @@ class ValidatorSelector:
                 metagraph.axons[self._next_uid].is_serving
                 and metagraph.S[self._next_uid] >= self._min_stake
                 and self._cooldowns.get(self._next_uid, 0) < current_time
-                and self.check_blacklist(self._next_uid)
+                and not self.check_blacklist(self._next_uid)
             ):
                 bt.logging.debug(f"Querying task from [{self._next_uid}]. Stake: {metagraph.S[self._next_uid]}")
                 return self._next_uid
